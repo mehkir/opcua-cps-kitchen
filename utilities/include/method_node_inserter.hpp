6c420ef157208bf80681c3d00a5b6f30144608ce
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-07-25 19:44:30 +0200

    Implement add method node methods

--- utilities/include/method_node_inserter.hpp
+++ utilities/include/method_node_inserter.hpp
@@ -7,14 +7,15 @@
 
 class method_node_inserter {
 private:
-    std::vector<UA_Argument> input_arguments;
-    std::vector<UA_Argument> output_arguments;
+    std::vector<UA_Argument> input_arguments_;
+    std::vector<UA_Argument> output_arguments_;
+    bool is_method_node_added_;
 public:
     method_node_inserter();
     ~method_node_inserter();
     void add_input_argument(std::string _description, std::string _name, UA_UInt32 _type_index);
     void add_output_argument(std::string _description, std::string _name, UA_UInt32 _type_index);
-    UA_StatusCode add_method_node(UA_Server* _server, std::string _method_node_id, std::string _browse_name, UA_MethodCallback _method_callback);
+    UA_StatusCode add_method_node(UA_Server* _server, UA_UInt32 _method_node_id, std::string _browse_name, UA_MethodCallback _method_callback);
 };
 
 #endif // METHOD_NODE_INSERTER_HPP
--- utilities/src/method_node_inserter.cpp
+++ utilities/src/method_node_inserter.cpp
@@ -1,19 +1,49 @@
 #include "../include/method_node_inserter.hpp"
 
-method_node_inserter::method_node_inserter() {
+method_node_inserter::method_node_inserter() : is_method_node_added_(false) {
 }
 
 method_node_inserter::~method_node_inserter() {
 }
 
 void method_node_inserter::add_input_argument(std::string _description, std::string _name, UA_UInt32 _type_index) {
-
+    UA_Argument input_argument;
+    UA_Argument_init(&input_argument);
+    input_argument.description = UA_LOCALIZEDTEXT("en-US", const_cast<char*>(_description.c_str()));
+    input_argument.name = UA_STRING(const_cast<char*>(_name.c_str()));
+    input_argument.dataType = UA_TYPES[_type_index].typeId;
+    input_argument.valueRank = UA_VALUERANK_SCALAR;
+    input_arguments_.push_back(input_argument);
 }
 
 void method_node_inserter::add_output_argument(std::string _description, std::string _name, UA_UInt32 _type_index) {
-
+    UA_Argument output_argument;
+    UA_Argument_init(&output_argument);
+    output_argument.description = UA_LOCALIZEDTEXT("en-US", const_cast<char*>(_description.c_str()));
+    output_argument.name = UA_STRING(const_cast<char*>(_name.c_str()));
+    output_argument.dataType = UA_TYPES[_type_index].typeId;
+    output_argument.valueRank = UA_VALUERANK_SCALAR;
+    output_arguments_.push_back(output_argument);
 }
 
-UA_StatusCode method_node_inserter::add_method_node(UA_Server* _server, std::string _method_node_id, std::string _browse_name, UA_MethodCallback _method_callback) {
-    return UA_STATUSCODE_GOOD;
+UA_StatusCode method_node_inserter::add_method_node(UA_Server* _server, UA_UInt32 _method_node_id, std::string _browse_name, UA_MethodCallback _method_callback) {
+    UA_StatusCode status_code = UA_STATUSCODE_BAD;
+    if (is_method_node_added_) {
+        return status_code;
+    }
+    UA_MethodAttributes method_attributes = UA_MethodAttributes_default;
+    std::string description = "desc.:" + _browse_name;
+    std::string display_name = "disp.:" + _browse_name;
+    method_attributes.description = UA_LOCALIZEDTEXT("en-US", const_cast<char*>(description.c_str()));
+    method_attributes.displayName = UA_LOCALIZEDTEXT("en-US", const_cast<char*>(display_name.c_str()));
+    method_attributes.executable = true;
+    method_attributes.userExecutable = true;
+    status_code = UA_Server_addMethodNode(_server, UA_NODEID_NUMERIC(1,_method_node_id),
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+                            UA_QUALIFIEDNAME(1, const_cast<char*>(_browse_name.c_str())),
+                            method_attributes, _method_callback,
+                            input_arguments_.size(), input_arguments_.data(), output_arguments_.size(), output_arguments_.data(), NULL, NULL);
+    is_method_node_added_ = true;
+    return status_code;
 }
