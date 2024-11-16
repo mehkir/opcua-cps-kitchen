8e990c929e954788c72b1f47d5845e1aebea7750
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-11-16 12:44:25 +0100

    Add conveyor port parameter

--- start_scripts/start_robot.bash
+++ start_scripts/start_robot.bash
@@ -2,4 +2,4 @@
 PROJECT_DIRECTORY=/home/mehmet/vscode-workspaces/opcua-cps-kitchen
 $PROJECT_DIRECTORY/build.bash
 # _robot_id, _robot_port, _clock_port, _controller_port, _conveyor_port
-$PROJECT_DIRECTORY/build/start_robot_instance 0 4000 5000 6000
+$PROJECT_DIRECTORY/build/start_robot_instance 0 4000 5000 6000 3000
