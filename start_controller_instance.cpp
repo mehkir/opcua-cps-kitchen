1d22e0df7b5cd68c700f7bf3da21b3e66142cb88
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-11-16 12:15:11 +0100

    Correct start scripts/code

--- start_controller_instance.cpp
+++ start_controller_instance.cpp
@@ -12,8 +12,8 @@ static void stop_handler(int sig) {
 int main(int argc, char* argv[]) {
     signal(SIGINT, stop_handler);
     signal(SIGTERM, stop_handler);
-    // _controller_port, _robot_start_port, _robot_count, _remote_conveyor_port, _conveyor_plates_count, _clock_port
-    controller controller_instance(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
+    // _controller_port, _robot_start_port, _robot_count, _remote_conveyor_port, _clock_port
+    controller controller_instance(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
     controller_instance_ = &controller_instance;
     controller_instance.start();
     return 0;
--- start_robot_instance.cpp
+++ start_robot_instance.cpp
@@ -13,8 +13,8 @@ int main(int argc, char* argv[]) {
     signal(SIGINT, stop_handler);
     signal(SIGTERM, stop_handler);
     
-    // _robot_id, _robot_port, _clock_port, _controller_port
-    robot robot_instance(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+    // _robot_id, _robot_port, _clock_port, _controller_port, _conveyor_port
+    robot robot_instance(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
     robot_instance_ = &robot_instance;
     robot_instance.start();
     return 0;
