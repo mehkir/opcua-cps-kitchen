66289bc79fb10b6244e63b57d647454557d49501
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-11-23 20:10:14 +0100

    Change client connection establisher to synchronized retries

--- wrappers/src/client_connection_establisher.cpp
+++ wrappers/src/client_connection_establisher.cpp
@@ -15,24 +15,24 @@ client_connection_establisher::~client_connection_establisher() {
 
 UA_SessionState client_connection_establisher::establish_connection(UA_Client* _client, UA_UInt16 _server_port) {
     UA_ClientConfig* client_config = UA_Client_getConfig(_client);
-    client_config->securityMode = UA_MESSAGESECURITYMODE_NONE;
     UA_ClientConfig_setDefault(client_config);
+    client_config->securityMode = UA_MESSAGESECURITYMODE_NONE;
+    client_config->timeout = 1000;
     std::string server_endpoint = "opc.tcp://localhost:" + std::to_string(_server_port);
 
     /* Connect to a server */
-    UA_SessionState session_state;
-    UA_SecureChannelState secure_channel_state;
+    UA_SessionState session_state = UA_SESSIONSTATE_CLOSED;
     int retry_counter = 0;
-    UA_Client_connectAsync(_client, server_endpoint.c_str());
-    do {
-        UA_Client_run_iterate(_client, 0);
-        UA_Client_getState(_client, &secure_channel_state, &session_state, NULL);
-        if (secure_channel_state == UA_SECURECHANNELSTATE_CLOSED) {
-            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Retrying to connect to the server on port %d", _server_port);
-            UA_Client_connectAsync(_client, server_endpoint.c_str());
+    while(retry_counter < RETRY_LIMIT) {
+        UA_StatusCode status_code = UA_Client_connect(_client, server_endpoint.c_str());
+        if(status_code != UA_STATUSCODE_GOOD) {
+            UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_CLIENT, "Not connected. Retrying to connect in 1 second");
             sleep_ms(1000);
             retry_counter++;
+            continue;
         }
-    } while (session_state != UA_SESSIONSTATE_ACTIVATED && retry_counter < RETRY_LIMIT);
+        session_state = UA_SESSIONSTATE_ACTIVATED;
+        break;
+    }
     return session_state;
 }
