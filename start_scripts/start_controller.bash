bdefbf5ccbad314731d3660308249fda6c423839
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-12-28 15:32:44 +0100

    Configure start scripts

--- start_scripts/start_controller.bash
+++ start_scripts/start_controller.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 PROJECT_DIRECTORY=/home/mehmet/vscode-workspaces/opcua-cps-kitchen
 $PROJECT_DIRECTORY/build.bash
-# _controller_port, _robot_start_port, _robot_count, _remote_conveyor_port
-$PROJECT_DIRECTORY/build/start_controller_instance 6000 4000 1 3000
+# _controller_port
+$PROJECT_DIRECTORY/build/start_controller_instance 5000
--- start_scripts/start_robot.bash
+++ start_scripts/start_robot.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 PROJECT_DIRECTORY=/home/mehmet/vscode-workspaces/opcua-cps-kitchen
 $PROJECT_DIRECTORY/build.bash
-# _robot_id, _robot_port, _clock_port, _controller_port, _conveyor_port
-$PROJECT_DIRECTORY/build/start_robot_instance 0 4000 5000 6000 3000
+# _robot_position, _robot_port, _controller_port, _conveyor_port
+$PROJECT_DIRECTORY/build/start_robot_instance 1 4000 5000 6000
