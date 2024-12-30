a89713eed8abc96d8ca0c119a35ac337d566d992
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-12-30 12:12:06 +0100

    Add information writer

---
+++ wrappers/include/information_node_writer.hpp
@@ -0,0 +1,13 @@
+#ifndef INFORMATION_NODE_WRITER_HPP
+#define INFORMATION_NODE_WRITER_HPP
+
+#include <open62541/server.h>
+
+class information_node_writer {
+    private:
+    public:
+        UA_StatusCode
+        write_value(UA_Server* _server, UA_NodeId _node_id, void* _value, const UA_DataType* _type);
+};
+
+#endif // INFORMATION_NODE_WRITER_HPP
