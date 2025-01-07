8b53d81e428925e7d1b09a347380c0d0201eb5f9
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-01-07 21:10:53 +0100

    Prepared recipe parser files for implementation

---
+++ recipe/include/recipe_parser.hpp
@@ -0,0 +1,11 @@
+#ifndef RECIPE_PARSER_HPP
+#define RECIPE_PARSER_HPP
+
+class recipe_parser {
+    private:
+    public:
+        recipe_parser();
+        ~recipe_parser();
+};
+
+#endif // RECIPE_PARSER_HPP
---
+++ recipe/src/recipe_parser.cpp
@@ -0,0 +1,8 @@
+#include "../include/recipe_parser.hpp"
+#include <jsoncpp/json/json.h>
+
+recipe_parser::recipe_parser() {
+}
+
+recipe_parser::~recipe_parser() {
+}
