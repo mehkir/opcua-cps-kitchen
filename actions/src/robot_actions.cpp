983b1372a5e747a42e147664f6b7b13d6f69ca0f
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-01-07 20:16:11 +0100

    Remove additional spaces

--- actions/src/robot_actions.cpp
+++ actions/src/robot_actions.cpp
@@ -44,12 +44,12 @@ robot_actions::robot_actions() {
     action_map_[BRAISE] = robot_action(BRAISE, robot_tools::PAN, BRAISING_TIME);
     action_map_[MASH] = robot_action(MASH, robot_tools::MASHER, MASHING_TIME);
     action_map_[STIR] = robot_action(STIR, robot_tools::STIRRER, STIRRING_TIME);
-    action_map_[SPRINKLE] =  robot_action(SPRINKLE, robot_tools::INGREDIENT_DISPENSER, SPRINKLING_TIME);
-    action_map_[POUR] =  robot_action(POUR, robot_tools::INGREDIENT_DISPENSER, POURING_TIME);
-    action_map_[WHIP] =  robot_action(WHIP, robot_tools::WHISK, WHIPPING_TIME);
-    action_map_[MIX] =  robot_action(MIX, robot_tools::MIXER, MIXING_TIME);
-    action_map_[CRUSH] =  robot_action(CRUSH, robot_tools::CRUSHER, CRUSHING_TIME);
-    action_map_[LAYER] =  robot_action(LAYER, robot_tools::LAYERING_DISPENSER, LAYERING_TIME);
+    action_map_[SPRINKLE] = robot_action(SPRINKLE, robot_tools::INGREDIENT_DISPENSER, SPRINKLING_TIME);
+    action_map_[POUR] = robot_action(POUR, robot_tools::INGREDIENT_DISPENSER, POURING_TIME);
+    action_map_[WHIP] = robot_action(WHIP, robot_tools::WHISK, WHIPPING_TIME);
+    action_map_[MIX] = robot_action(MIX, robot_tools::MIXER, MIXING_TIME);
+    action_map_[CRUSH] = robot_action(CRUSH, robot_tools::CRUSHER, CRUSHING_TIME);
+    action_map_[LAYER] = robot_action(LAYER, robot_tools::LAYERING_DISPENSER, LAYERING_TIME);
     // recipe timed actions
     action_map_[BOIL] = robot_action(BOIL, robot_tools::POT);
     action_map_[BAKE] = robot_action(BAKE, robot_tools::OVEN);
