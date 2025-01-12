ec6232f00af128e2c0a437b1ddfa370ad3aeb7bc
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-01-12 11:31:28 +0100

    Correct handover finished order

--- node_ids.hpp
+++ node_ids.hpp
@@ -3,9 +3,13 @@
 
 /* ROBOT */
 #define RECEIVE_TASK "receive_task"
-#define HANDOVER_FINSIHED_ORDER "handover_finished_order"
+#define HANDOVER_FINISHED_ORDER "handover_finished_order"
 #define ROBOT_STATE "robot_state"
 #define RECIPE_ID "recipe_id"
+#define DISH_NAME "dish_name"
+#define ACTION_NAME "action_name"
+#define INGREDIENTS "ingredients"
+#define OVERALL_TIME "overall_time"
 
 /* CONVEYOR */
 #define FINISHED_ORDER_NOTIFICATION "finished_order_notification"
