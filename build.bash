20fbef0f925b329d6c2d9565f8dfd7a9391a7fb9
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-01-13 12:59:18 +0100

    Uncomment robot demo

--- build.bash
+++ build.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 cmake -B /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build -S /home/mehmet/vscode-workspaces/opcua-cps-kitchen
-# $(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target robot_demo -- -j$(nproc)
+$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target robot_demo -- -j$(nproc)
 $(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target clock_demo -- -j$(nproc)
 $(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target string_server_for_subscription -- -j$(nproc)
 $(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target string_client_for_subscription -- -j$(nproc)
