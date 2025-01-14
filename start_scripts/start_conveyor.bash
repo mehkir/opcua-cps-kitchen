8abb253feb7453b8c4513336383c8417c35f3afd
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-01-14 01:01:01 +0100

    Modify conveyor script

--- start_scripts/start_conveyor.bash
+++ start_scripts/start_conveyor.bash
@@ -2,4 +2,4 @@
 PROJECT_DIRECTORY=/home/mehmet/vscode-workspaces/opcua-cps-kitchen
 $PROJECT_DIRECTORY/build.bash
 # _conveyor_port, _robot_count
-$PROJECT_DIRECTORY/build/start_conveyor_instance 6000 1
+$PROJECT_DIRECTORY/build/start_conveyor_instance 6000 4
