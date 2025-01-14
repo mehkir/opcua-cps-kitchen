28e6dbaff820ff1f52e3a280cd9994c33427ffaf
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-01-14 01:00:11 +0100

    Start multiple robots

--- start_scripts/start_robot.bash
+++ start_scripts/start_robot.bash
@@ -2,4 +2,23 @@
 PROJECT_DIRECTORY=/home/mehmet/vscode-workspaces/opcua-cps-kitchen
 $PROJECT_DIRECTORY/build.bash
 # _robot_position, _robot_port, _controller_port, _conveyor_port
-$PROJECT_DIRECTORY/build/start_robot_instance 1 4000 5000 6000
+# $PROJECT_DIRECTORY/build/start_robot_instance 1 4000 5000 6000
+ROBOTS=4
+START_PORT=4000
+CONTROLLER_PORT=5000
+CONVEYOR_PORT=6000
+
+for ((robot_count = 0; robot_count < ROBOTS; robot_count++)); do
+    robot_position=$(( $robot_count + 1 ))
+    robot_port=$(( $START_PORT + $robot_count))
+    echo "Starting robot with port $robot_port at position $robot_position"
+    $PROJECT_DIRECTORY/build/start_robot_instance $robot_position $robot_port $CONTROLLER_PORT $CONVEYOR_PORT & 1>/dev/null
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Error: Non-zero exit code detected. Exiting."
+        break
+    fi
+done
+echo "All robots started"
+wait
+echo "All robots terminated"
