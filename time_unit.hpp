0af65823ca40a8156a58befb60cb27d4753752ef
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-01-27 20:09:28 +0100

    Add time unit for seconds

--- time_unit.hpp
+++ time_unit.hpp
@@ -3,6 +3,7 @@
 
 #include <open62541/types.h>
 
-#define TIME_UNIT (UA_DATETIME_MSEC * 100LL)
+// #define TIME_UNIT (UA_DATETIME_MSEC * 100LL)
+#define TIME_UNIT UA_DATETIME_SEC
 
 #endif // TIME_UNIT_HPP
