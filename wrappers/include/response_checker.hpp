37f9dea8efead657535b63184dda3529fafdc345
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-03-21 12:08:47 +0100

    Add doxygen comments

--- wrappers/include/response_checker.hpp
+++ wrappers/include/response_checker.hpp
@@ -7,12 +7,49 @@ class response_checker {
 private:
     const UA_CallResponse& response_;
 public:
+    /**
+     * @brief Constructs a new response checker instance
+     * 
+     * @param _response the response to be checked
+     */
     response_checker(UA_CallResponse* _response);
+
+    /**
+     * @brief Destructs the response checker instance
+     * 
+     */
     ~response_checker();
+
     bool has_scalar_type(size_t _results_index, size_t _output_index, const UA_DataType* _type);
+    /**
+     * @brief Returns the size of the results array
+     * 
+     * @return size_t the size of the results array
+     */
     size_t get_results_size();
+
+    /**
+     * @brief Returns the size of the output array at the given results array index
+     * 
+     * @param _results_index the results index
+     * @return size_t the size of the output array
+     */
     size_t get_output_arguments_size(size_t _results_index);
+
+    /**
+     * @brief Returns the pointer to the output data at the given indices
+     * 
+     * @param _results_index the results index for the output array in the results array
+     * @param _output_index the index of the data in the output array
+     * @return void* the pointer to the data at the given indices
+     */
     void* get_data(size_t _results_index, size_t _output_index);
+
+    /**
+     * @brief Returns the service result of the response
+     * 
+     * @return UA_StatusCode 
+     */
     UA_StatusCode get_service_result();
 };
 
