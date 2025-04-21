e95d3201a8f9e14c4450dc318fcd43e7eb1512bc
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-04-22 01:41:30 +0200

    Set time unit to 100msec

--- time_unit.hpp
+++ time_unit.hpp
@@ -3,8 +3,9 @@
 
 #include <open62541/types.h>
 
-// #define TIME_UNIT (UA_DATETIME_MSEC * 100LL)
-#define TIME_UNIT UA_DATETIME_SEC
+// #define TIME_UNIT UA_DATETIME_MSEC
+#define TIME_UNIT (UA_DATETIME_MSEC * 100LL)
+// #define TIME_UNIT UA_DATETIME_SEC
 #define TIME_UNIT_UPDATE_RATE 1
 
 #endif // TIME_UNIT_HPP
