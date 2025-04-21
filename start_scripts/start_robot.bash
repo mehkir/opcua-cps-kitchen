b5b52862facf9b4173cddbe05993e4d67d5b89d8
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-04-22 01:41:54 +0200

    Redirect log to dev null

--- start_scripts/start_robot.bash
+++ start_scripts/start_robot.bash
@@ -12,6 +12,7 @@ for ((robot_count = 0; robot_count < ROBOTS; robot_count++)); do
     robot_position=$(( $robot_count + 1 ))
     robot_port=$(( $START_PORT + $robot_count))
     echo "Starting robot with port $robot_port at position $robot_position"
+    # $PROJECT_DIRECTORY/build/start_robot_instance $robot_position $robot_port $CONTROLLER_PORT $CONVEYOR_PORT &
     $PROJECT_DIRECTORY/build/start_robot_instance $robot_position $robot_port $CONTROLLER_PORT $CONVEYOR_PORT 1>/dev/null &
     # $PROJECT_DIRECTORY/build/start_robot_instance $robot_position $robot_port $CONTROLLER_PORT $CONVEYOR_PORT >./logs/robot_${robot_position}_${robot_port}_${ROBOTS}_$(date +%Y%m%d%H%M%S) &
     exit_code=$?
