f0c5983a037f8a23c9e7dbb7258f70c66b540d7a
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-06-26 18:59:41 +0200

    Implement minimal discovery demo

---
+++ demos/client_discovery_lookup_minimal.cpp
@@ -0,0 +1,86 @@
+/* This work is licensed under a Creative Commons CCZero 1.0 Universal License.
+ * See http://creativecommons.org/publicdomain/zero/1.0/ for more information. */
+/**
+ * This client requests all the available servers from the discovery server (see server_lds.c)
+ * and then calls GetEndpoints on the returned list of servers.
+ */
+
+#include <open62541/client_config_default.h>
+#include <open62541/client_highlevel.h>
+#include <open62541/plugin/log_stdout.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DISCOVERY_SERVER_ENDPOINT "opc.tcp://localhost:4840"
+
+int main(void) {
+    /* Example for calling FindServers */
+    UA_ApplicationDescription *applicationDescriptionArray = NULL;
+    size_t applicationDescriptionArraySize = 0;
+
+    UA_StatusCode retval;
+    {
+        UA_Client *client = UA_Client_new();
+        UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+        retval = UA_Client_findServers(client, DISCOVERY_SERVER_ENDPOINT, 0, NULL, 0, NULL,
+                                       &applicationDescriptionArraySize, &applicationDescriptionArray);
+        UA_Client_delete(client);
+    }
+    if(retval != UA_STATUSCODE_GOOD) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Could not call FindServers service. "
+                "Is the discovery server started? StatusCode %s", UA_StatusCode_name(retval));
+        return EXIT_FAILURE;
+    }
+
+    /*
+     * Now that we have the list of available servers, call get endpoints on all of them
+     */
+    for(size_t i = 0; i < applicationDescriptionArraySize; i++) {
+        UA_ApplicationDescription *description = &applicationDescriptionArray[i];
+        if(description->discoveryUrlsSize == 0) {
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_CLIENT,
+                        "[GetEndpoints] Server %.*s did not provide any discovery urls. Skipping.",
+                        (int)description->applicationUri.length, description->applicationUri.data);
+            continue;
+        }
+
+        if(description->applicationType != UA_APPLICATIONTYPE_SERVER)
+            continue;
+
+        printf("\nEndpoints for Server[%lu]: %.*s\n", (unsigned long) i,
+               (int) description->applicationUri.length, description->applicationUri.data);
+
+        UA_Client *client = UA_Client_new();
+        UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+
+        char *discoveryUrl = (char *) UA_malloc(sizeof(char) * description->discoveryUrls[0].length + 1);
+        memcpy(discoveryUrl, description->discoveryUrls[0].data, description->discoveryUrls[0].length);
+        discoveryUrl[description->discoveryUrls[0].length] = '\0';
+
+        UA_EndpointDescription *endpointArray = NULL;
+        size_t endpointArraySize = 0;
+        retval = UA_Client_getEndpoints(client, discoveryUrl, &endpointArraySize, &endpointArray);
+        UA_free(discoveryUrl);
+        if(retval != UA_STATUSCODE_GOOD) {
+            UA_Client_disconnect(client);
+            UA_Client_delete(client);
+            break;
+        }
+
+        for(size_t j = 0; j < endpointArraySize; j++) {
+            UA_EndpointDescription *endpoint = &endpointArray[j];
+            printf("\n\t\tEndpoint URL: %.*s", (int) endpoint->endpointUrl.length, endpoint->endpointUrl.data);
+        }
+
+        UA_Array_delete(endpointArray, endpointArraySize, &UA_TYPES[UA_TYPES_ENDPOINTDESCRIPTION]);
+        UA_Client_delete(client);
+    }
+
+    printf("\n");
+
+    UA_Array_delete(applicationDescriptionArray, applicationDescriptionArraySize,
+                    &UA_TYPES[UA_TYPES_APPLICATIONDESCRIPTION]);
+
+    return EXIT_SUCCESS;
+}
