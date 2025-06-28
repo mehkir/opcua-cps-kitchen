9ff4de4bfdf5b59b1dc214ee62578ed2123ded90
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-06-28 17:44:03 +0200

    Add upper camel case browsenames

---
+++ browsenames.h
@@ -0,0 +1,31 @@
+#ifndef NODE_IDS_HPP
+#define NODE_IDS_HPP
+
+/* ROBOT */
+// object type node
+#define ROBOT_TYPE "RobotType"
+// method nodes
+#define RECEIVE_TASK "ReceiveTask"
+#define HANDOVER_FINISHED_ORDER "HandoverFinishedOrder"
+// information nodes
+#define AGENT_TYPE "AgentType"
+#define RECIPE_ID "RecipeId"
+#define DISH_NAME "DishName"
+#define ACTION_NAME "ActionName"
+#define INGREDIENTS "Ingredients"
+#define OVERALL_TIME "OverallTime"
+#define CURRENT_TOOL "CurrentTool"
+#define LAST_EQUIPPED_TOOL "LastEquippedTool"
+#define CAPABILITIES "Capabilities"
+
+/* CONVEYOR */
+// method nodes
+#define FINISHED_ORDER_NOTIFICATION "FinishedOrderNotification"
+
+/* CONTROLLER */
+// method nodes
+#define REGISTER_ROBOT "RegisterRobot"
+#define CHOOSE_NEXT_ROBOT "ChooseNextRobot"
+#define PLACE_RANDOM_ORDER "PlaceRandomOrder"
+
+#endif // NODE_IDS_HPP
