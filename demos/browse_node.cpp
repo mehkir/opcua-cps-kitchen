317bd189bbdbbef3f2c4f617eff884d1bdd335e2
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-06-29 21:29:54 +0200

    Use browse description, extract browse steps in separate methods

--- demos/browse_node.cpp
+++ demos/browse_node.cpp
@@ -1,9 +1,7 @@
-#include <stdio.h>
 #include <open62541/client_config_default.h>
 #include <open62541/plugin/log_stdout.h>
 #include <signal.h>
 #include <string>
-#include <vector>
 #include <iostream>
 
 #include "client_connection_establisher.hpp"
@@ -16,61 +14,62 @@ static void stopHandler(int sig) {
 }
 
 
-std::vector<UA_NodeId> browse(UA_Client* _client, UA_NodeId _start_node_id, uint32_t _target_namespace = 0, std::string _target_browsename = "") {
-    UA_BrowseRequest bReq;
-    UA_BrowseRequest_init(&bReq);
-    bReq.requestedMaxReferencesPerNode = 0;
-    bReq.nodesToBrowse = UA_BrowseDescription_new();
-    bReq.nodesToBrowseSize = 1;
-    bReq.nodesToBrowse[0].nodeId = _start_node_id;
-    bReq.nodesToBrowse[0].resultMask = UA_BROWSERESULTMASK_ALL;
-    bReq.nodesToBrowse[0].browseDirection = UA_BROWSEDIRECTION_FORWARD;
-    bReq.nodesToBrowse[0].referenceTypeId = UA_NODEID_NULL;
-    bReq.nodesToBrowse[0].includeSubtypes = true;
+UA_NodeId browse_object_type(UA_Client* _client, UA_NodeId _start_node_id, std::string _target_browsename) {
+    UA_BrowseDescription bd;
+    UA_BrowseDescription_init(&bd);
+    bd.nodeId = _start_node_id;
+    bd.referenceTypeId = UA_NS0ID(HASSUBTYPE);
+    bd.browseDirection = UA_BROWSEDIRECTION_FORWARD;
+    bd.includeSubtypes = true;
+    bd.nodeClassMask = UA_NODECLASS_OBJECTTYPE;
+    bd.resultMask = UA_BROWSERESULTMASK_BROWSENAME;
 
-    UA_BrowseResponse bResp = UA_Client_Service_browse(_client, bReq);
+    UA_BrowseResult bres = UA_Client_browse(_client, NULL, 0, &bd);
+    UA_NodeId matching_node_id = UA_NODEID_NULL;
+    for(size_t i = 0; i < bres.referencesSize; ++i) {
+        const UA_ReferenceDescription *ref = &(bres.references[i]);
+        std::string browse_name((char*) ref->browseName.name.data, ref->browseName.name.length);
+        if (!browse_name.compare(_target_browsename))
+            matching_node_id = ref->nodeId.nodeId;
+    }
 
-    std::vector<UA_NodeId> matching_node_ids;
-    /* Iterate results */
-    for(size_t i = 0; i < bResp.resultsSize; ++i) {
-        for(size_t j = 0; j < bResp.results[i].referencesSize; ++j) {
-            UA_ReferenceDescription *ref = &(bResp.results[i].references[j]);
+    UA_BrowseResult_clear(&bres);
+    return matching_node_id;
+}
 
-            UA_NodeId organizesId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
-            UA_NodeId hasComponentId = UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT);
-            UA_NodeId hasPropertyId = UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY);
+void browse_objects(UA_Client* _client, UA_BrowseResult& _browse_result) {
+    UA_BrowseDescription bd;
+    UA_BrowseDescription_init(&bd);
+    bd.nodeId = UA_NS0ID(OBJECTSFOLDER);
+    bd.referenceTypeId = UA_NS0ID(ORGANIZES);
+    bd.browseDirection = UA_BROWSEDIRECTION_FORWARD;
+    bd.includeSubtypes = true;
+    bd.nodeClassMask = UA_NODECLASS_OBJECT;
+    bd.resultMask = UA_BROWSERESULTMASK_ALL;
+    _browse_result = UA_Client_browse(_client, NULL, 0, &bd);
+}
 
-            bool is_allowed_ref_type =
-                UA_NodeId_equal(&ref->referenceTypeId, &organizesId) ||
-                UA_NodeId_equal(&ref->referenceTypeId, &hasComponentId) ||
-                UA_NodeId_equal(&ref->referenceTypeId, &hasPropertyId);
+UA_NodeId browse_instance(UA_Client* _client, UA_NodeId _start_node_id, UA_NodeId _target_type_node_id) {
+    UA_BrowseDescription bd;
+    UA_BrowseDescription_init(&bd);
+    bd.nodeId = _start_node_id;
+    bd.referenceTypeId = UA_NS0ID(HASTYPEDEFINITION);
+    bd.browseDirection = UA_BROWSEDIRECTION_FORWARD;
+    bd.includeSubtypes = true;
+    bd.resultMask = UA_BROWSERESULTMASK_ALL;
 
-            if (!is_allowed_ref_type)
-                continue;
-
-            printf("Node BrowseName: %.*s\n",
-               (int)ref->browseName.name.length, ref->browseName.name.data);
-            printf("Node DisplayName: %.*s\n",
-               (int)ref->displayName.text.length, ref->displayName.text.data);
-            
-            if (_target_browsename.empty()) {
-                matching_node_ids.push_back(ref->nodeId.nodeId);
-                continue;
-            }
-            std::string browsename((char*) ref->browseName.name.data, ref->browseName.name.length);
-            if (ref->browseName.namespaceIndex == _target_namespace && !(browsename.compare(_target_browsename))) {
-                // Match found
-                UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "%d" , (UA_UInt32) ref->nodeId.nodeId.identifier.numeric);
-                matching_node_ids.push_back(ref->nodeId.nodeId);
-            }
+    UA_BrowseResult bres = UA_Client_browse(_client, NULL, 0, &bd);
+    UA_NodeId matching_node_id = UA_NODEID_NULL;
+    if (bres.referencesSize > 0) {
+        UA_NodeId* type_node_id = &bres.references[0].nodeId.nodeId;
+        if (UA_NodeId_equal(type_node_id, &_target_type_node_id)) {
+            matching_node_id = _start_node_id;
         }
     }
 
-    UA_BrowseRequest_clear(&bReq);
-    UA_BrowseResponse_clear(&bResp);
-    return matching_node_ids;
+    UA_BrowseResult_clear(&bres);
+    return matching_node_id;
 }
-
 
 
 int main(int argc, char* argv[]) {
@@ -79,31 +78,31 @@ int main(int argc, char* argv[]) {
 
     UA_Client* client = UA_Client_new();
     client_connection_establisher con_estab;
-    UA_SessionState session_state = con_estab.establish_connection(client, 4840);
+    UA_SessionState session_state = con_estab.establish_connection(client, 5000);
     if (session_state != UA_SESSIONSTATE_ACTIVATED) {
         UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SESSION, "%s: Error establishing client session", __FUNCTION__);
         running = false;
         return UA_STATUSCODE_BAD;
     }
 
-    std::vector<UA_NodeId> object_ids = browse(client, UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER), 1, "Pump (Manual)");
-    for (UA_NodeId nid : object_ids) {
-        std::cout << "Found object id: " << nid.namespaceIndex << "," << nid.identifier.numeric << std::endl;
-        std::vector<UA_NodeId> attribute_ids = browse(client, nid);
-        for (UA_NodeId nid : attribute_ids) {
-            std::cout << "Found attribure id: " << nid.namespaceIndex << "," << nid.identifier.numeric << std::endl;
-            information_node_reader nreader;
-            UA_StatusCode status = nreader.read_information_node(client, nid);
-            if (status != UA_STATUSCODE_GOOD) {
-                UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Failed to read information node");
-                continue;
-            }
-            UA_String string_value = *(UA_String*) nreader.get_variant()->data;
-            std::string new_str = std::string((char*) string_value.data, string_value.length);
-            std::cout << "Attribute value: " << new_str << std::endl;
+    UA_NodeId node_id = browse_object_type(client, UA_NS0ID(BASEOBJECTTYPE), "ControllerType");
+    std::cout << "ControllerType Node id(" << node_id.namespaceIndex << "," << node_id.identifier.numeric << ")" << std::endl;
+
+    UA_BrowseResult browse_result;
+    browse_objects(client, browse_result);
+
+    for (size_t i = 0; i < browse_result.referencesSize; i++) {
+        const UA_ReferenceDescription* reference_description = &browse_result.references[i];
+        UA_NodeId instance_node_id = browse_instance(client, reference_description->nodeId.nodeId, node_id);
+        if (!UA_NodeId_equal(&instance_node_id, &UA_NODEID_NULL)) {
+            UA_QualifiedName instance_browse_name;
+            UA_Client_readBrowseNameAttribute(client, instance_node_id, &instance_browse_name);
+            std::string bm((char*) instance_browse_name.name.data, instance_browse_name.name.length);
+            std::cout << bm << " Node id(" << instance_node_id.namespaceIndex << "," << instance_node_id.identifier.numeric << ")" << std::endl;
         }
     }
 
+    UA_BrowseResult_clear(&browse_result);
     UA_Client_delete(client);
     return 0;
 }
