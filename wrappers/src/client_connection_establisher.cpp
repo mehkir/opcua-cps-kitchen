2056a320e1cda4f5368b3cfa87e99e93dbee6c68
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-07-04 22:52:35 +0200

    Remove old code

--- wrappers/src/client_connection_establisher.cpp
+++ wrappers/src/client_connection_establisher.cpp
@@ -24,20 +24,6 @@ client_connection_establisher::establish_connection(std::string _server_endpoint
     client_config->stateCallback = connection_status_callback;
     // client_config->timeout = 1000;
     // *client_config->logging = filtered_logger().create_filtered_logger(UA_LOGLEVEL_INFO, UA_LOGCATEGORY_USERLAND);
-    // std::string server_endpoint = "opc.tcp://localhost:" + std::to_string(_server_port);
-
-    /* Connect to a server */
-    // UA_SessionState session_state = UA_SESSIONSTATE_CLOSED;
-    // int retry_counter = 0;
-    // while(retry_counter < RETRY_LIMIT && session_state != UA_SESSIONSTATE_ACTIVATED) {
-    //     UA_StatusCode status_code = UA_Client_connect(_client, _server_endpoint.c_str());
-    //     UA_Client_getState(_client, NULL, &session_state, NULL);
-    //     if(status_code != UA_STATUSCODE_GOOD || session_state != UA_SESSIONSTATE_ACTIVATED) {
-    //         UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Not connected. Retrying to connect in 1 second");
-    //         sleep_ms(1000);
-    //         retry_counter++;
-    //     }
-    // }
     auto start = std::chrono::steady_clock::now();
     while(!connected_) {
         UA_Client_connectAsync(client_, _server_endpoint.c_str());
