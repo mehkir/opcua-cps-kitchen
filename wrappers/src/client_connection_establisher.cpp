c5e917d786bb7807f4effb3a4c086032ea29bc6f
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-07-04 22:50:15 +0200

    Fix reconnection

--- wrappers/src/client_connection_establisher.cpp
+++ wrappers/src/client_connection_establisher.cpp
@@ -22,7 +22,7 @@ client_connection_establisher::establish_connection(std::string _server_endpoint
     UA_ClientConfig_setDefault(client_config);
     client_config->securityMode = UA_MESSAGESECURITYMODE_NONE;
     client_config->stateCallback = connection_status_callback;
-    client_config->timeout = 1000;
+    // client_config->timeout = 1000;
     // *client_config->logging = filtered_logger().create_filtered_logger(UA_LOGLEVEL_INFO, UA_LOGCATEGORY_USERLAND);
     // std::string server_endpoint = "opc.tcp://localhost:" + std::to_string(_server_port);
 
@@ -38,10 +38,9 @@ client_connection_establisher::establish_connection(std::string _server_endpoint
     //         retry_counter++;
     //     }
     // }
-
-    UA_Client_connectAsync(client_, _server_endpoint.c_str());
     auto start = std::chrono::steady_clock::now();
     while(!connected_) {
+        UA_Client_connectAsync(client_, _server_endpoint.c_str());
         UA_StatusCode status = UA_Client_run_iterate(client_, 100);
         if (status != UA_STATUSCODE_GOOD) {
             UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "%s: %s", __FUNCTION__, UA_StatusCode_name(status));
