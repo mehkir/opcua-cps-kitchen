b90383d2dc1a3577a784246af75fcf336f7564e4
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-07-05 21:59:45 +0200

    Simplify client connection establisher

--- wrappers/include/client_connection_establisher.hpp
+++ wrappers/include/client_connection_establisher.hpp
@@ -8,14 +8,10 @@
 class client_connection_establisher {
 private:
     UA_Client* client_;
-    static std::unordered_map<UA_Client*, client_connection_establisher*> client_map_;
-    bool connected_;
 public:
     client_connection_establisher(UA_Client* _client);
     ~client_connection_establisher();
     bool establish_connection(std::string _server_endpoint);
-    void static connection_status_callback(UA_Client* _client, UA_SecureChannelState _channel_state, UA_SessionState _session_state, UA_StatusCode _connect_status);
-    void on_connection_status(UA_Client* _client, UA_SecureChannelState _channel_state, UA_SessionState _session_state, UA_StatusCode _connect_status);
 };
 
 
--- wrappers/src/client_connection_establisher.cpp
+++ wrappers/src/client_connection_establisher.cpp
@@ -6,14 +6,10 @@
 
 #define TIMEOUT 10
 
-std::unordered_map<UA_Client*, client_connection_establisher*> client_connection_establisher::client_map_;
-
-client_connection_establisher::client_connection_establisher(UA_Client* _client) : client_(_client), connected_(false) {
-    client_map_[client_] = this;
+client_connection_establisher::client_connection_establisher(UA_Client* _client) : client_(_client) {
 }
 
 client_connection_establisher::~client_connection_establisher() {
-    client_map_.erase(client_);
 }
 
 bool
@@ -21,16 +17,17 @@ client_connection_establisher::establish_connection(std::string _server_endpoint
     UA_ClientConfig* client_config = UA_Client_getConfig(client_);
     UA_ClientConfig_setDefault(client_config);
     client_config->securityMode = UA_MESSAGESECURITYMODE_NONE;
-    client_config->stateCallback = connection_status_callback;
     // client_config->timeout = 1000;
     // *client_config->logging = filtered_logger().create_filtered_logger(UA_LOGLEVEL_INFO, UA_LOGCATEGORY_USERLAND);
     auto start = std::chrono::steady_clock::now();
-    while(!connected_) {
+    UA_SessionState session_state = UA_SESSIONSTATE_CLOSED;
+    while(session_state != UA_SESSIONSTATE_ACTIVATED) {
         UA_Client_connectAsync(client_, _server_endpoint.c_str());
         UA_StatusCode status = UA_Client_run_iterate(client_, 100);
         if (status != UA_STATUSCODE_GOOD) {
             UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "%s: %s", __FUNCTION__, UA_StatusCode_name(status));
         }
+        UA_Client_getState(client_, NULL, &session_state, NULL);
         auto now = std::chrono::steady_clock::now();
         auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - start).count();
         if (elapsed >= TIMEOUT) {
@@ -38,22 +35,5 @@ client_connection_establisher::establish_connection(std::string _server_endpoint
             break;
         }
     }
-    return connected_;
-}
-
-void 
-client_connection_establisher::connection_status_callback(UA_Client* _client, UA_SecureChannelState _channel_state, UA_SessionState _session_state, UA_StatusCode _connect_status) {
-    if (client_map_.find(_client) != client_map_.end()) {
-        client_connection_establisher* instance = client_map_[_client];
-        instance->on_connection_status(_client, _channel_state, _session_state, _connect_status);
-    }
-}
-
-void client_connection_establisher::on_connection_status(UA_Client* _client, UA_SecureChannelState _channel_state, UA_SessionState _session_state, UA_StatusCode _connect_status) {
-    if(_session_state == UA_SESSIONSTATE_ACTIVATED)
-        connected_ = true;
-    else
-        connected_ = false;
-    if (connected_)
-        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "%s: connected %d", __FUNCTION__, connected_);
+    return session_state == UA_SESSIONSTATE_ACTIVATED;
 }
