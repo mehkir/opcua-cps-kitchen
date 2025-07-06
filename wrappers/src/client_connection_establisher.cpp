f907f94aadd97cdeacb00ea64bdfed9045a17e1a
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-07-06 22:43:41 +0200

    Set timeout to 1 second, add retry log

--- wrappers/src/client_connection_establisher.cpp
+++ wrappers/src/client_connection_establisher.cpp
@@ -17,17 +17,17 @@ client_connection_establisher::establish_connection(std::string _server_endpoint
     UA_ClientConfig* client_config = UA_Client_getConfig(client_);
     UA_ClientConfig_setDefault(client_config);
     client_config->securityMode = UA_MESSAGESECURITYMODE_NONE;
-    client_config->timeout = 0;
+    client_config->timeout = 1000;
     // *client_config->logging = filtered_logger().create_filtered_logger(UA_LOGLEVEL_INFO, UA_LOGCATEGORY_USERLAND);
 
     auto start = std::chrono::steady_clock::now();
     UA_SessionState session_state = UA_SESSIONSTATE_CLOSED;
     while(session_state != UA_SESSIONSTATE_ACTIVATED) {
         UA_StatusCode status = UA_Client_connect(client_, _server_endpoint.c_str());
-        if (status != UA_STATUSCODE_GOOD) {
-            UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "%s: %s", __FUNCTION__, UA_StatusCode_name(status));
-        }
         UA_Client_getState(client_, NULL, &session_state, NULL);
+        if (session_state != UA_SESSIONSTATE_ACTIVATED) {
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "%s: Connection attempt failed. Retrying to connect in 1 second", __FUNCTION__);
+        }
         auto now = std::chrono::steady_clock::now();
         auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - start).count();
         if (elapsed >= TIMEOUT) {
