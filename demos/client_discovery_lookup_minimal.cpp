311c2c89d51c76ab82f83d0da2cc7644609e0ab0
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-07-15 22:25:27 +0200

    Change discovery lookup to c++, use string instead malloc and memcpy

--- demos/client_discovery_lookup_minimal.cpp
+++ demos/client_discovery_lookup_minimal.cpp
@@ -11,20 +11,21 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
 
 #define DISCOVERY_SERVER_ENDPOINT "opc.tcp://localhost:4840"
 
 int main(void) {
     /* Example for calling FindServers */
-    UA_ApplicationDescription *applicationDescriptionArray = NULL;
-    size_t applicationDescriptionArraySize = 0;
+    UA_ApplicationDescription* application_description_array = NULL;
+    size_t application_description_array_size = 0;
 
     UA_StatusCode retval;
     {
-        UA_Client *client = UA_Client_new();
+        UA_Client* client = UA_Client_new();
         UA_ClientConfig_setDefault(UA_Client_getConfig(client));
         retval = UA_Client_findServers(client, DISCOVERY_SERVER_ENDPOINT, 0, NULL, 0, NULL,
-                                       &applicationDescriptionArraySize, &applicationDescriptionArray);
+                                       &application_description_array_size, &application_description_array);
         UA_Client_delete(client);
     }
     if(retval != UA_STATUSCODE_GOOD) {
@@ -36,8 +37,8 @@ int main(void) {
     /*
      * Now that we have the list of available servers, call get endpoints on all of them
      */
-    for(size_t i = 0; i < applicationDescriptionArraySize; i++) {
-        UA_ApplicationDescription *description = &applicationDescriptionArray[i];
+    for(size_t i = 0; i < application_description_array_size; i++) {
+        UA_ApplicationDescription* description = &application_description_array[i];
         if(description->discoveryUrlsSize == 0) {
             UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_CLIENT,
                         "[GetEndpoints] Server %.*s did not provide any discovery urls. Skipping.",
@@ -54,30 +55,31 @@ int main(void) {
         UA_Client *client = UA_Client_new();
         UA_ClientConfig_setDefault(UA_Client_getConfig(client));
 
-        char *discoveryUrl = (char *) UA_malloc(sizeof(char) * description->discoveryUrls[0].length + 1);
-        memcpy(discoveryUrl, description->discoveryUrls[0].data, description->discoveryUrls[0].length);
-        discoveryUrl[description->discoveryUrls[0].length] = '\0';
+        // char *discoveryUrl = (char *) UA_malloc(sizeof(char) * description->discoveryUrls[0].length + 1);
+        // memcpy(discoveryUrl, description->discoveryUrls[0].data, description->discoveryUrls[0].length);
+        // discoveryUrl[description->discoveryUrls[0].length] = '\0';
 
-        UA_EndpointDescription *endpointArray = NULL;
-        size_t endpointArraySize = 0;
-        retval = UA_Client_getEndpoints(client, discoveryUrl, &endpointArraySize, &endpointArray);
-        UA_free(discoveryUrl);
+        std::string discovery_url((char*) description->discoveryUrls[0].data, description->discoveryUrls[0].length);
+        UA_EndpointDescription* enpoint_array = NULL;
+        size_t enpoint_array_size = 0;
+        retval = UA_Client_getEndpoints(client, discovery_url.c_str(), &enpoint_array_size, &enpoint_array);
+        // UA_free(discoveryUrl);
         if(retval != UA_STATUSCODE_GOOD) {
             UA_Client_disconnect(client);
             UA_Client_delete(client);
             break;
         }
 
-        for(size_t j = 0; j < endpointArraySize; j++) {
-            UA_EndpointDescription *endpoint = &endpointArray[j];
+        for(size_t j = 0; j < enpoint_array_size; j++) {
+            UA_EndpointDescription *endpoint = &enpoint_array[j];
             printf("Endpoint URL: %.*s\n", (int) endpoint->endpointUrl.length, endpoint->endpointUrl.data);
         }
 
-        UA_Array_delete(endpointArray, endpointArraySize, &UA_TYPES[UA_TYPES_ENDPOINTDESCRIPTION]);
+        UA_Array_delete(enpoint_array, enpoint_array_size, &UA_TYPES[UA_TYPES_ENDPOINTDESCRIPTION]);
         UA_Client_delete(client);
     }
 
-    UA_Array_delete(applicationDescriptionArray, applicationDescriptionArraySize,
+    UA_Array_delete(application_description_array, application_description_array_size,
                     &UA_TYPES[UA_TYPES_APPLICATIONDESCRIPTION]);
 
     return EXIT_SUCCESS;
