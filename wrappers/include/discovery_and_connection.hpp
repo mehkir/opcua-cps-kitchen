58301423c0d1b1ba2cd1741b751b4bb25ad3db93
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-09-06 00:50:51 +0200

    Add doxygen comment

--- wrappers/include/discovery_and_connection.hpp
+++ wrappers/include/discovery_and_connection.hpp
@@ -6,6 +6,15 @@
 #include <vector>
 #include "../include/discovery_util.hpp"
 
+/**
+ * @brief Discovers and connects to the first endpoint it finds with an instance of the given object type
+ * 
+ * @param _client the client
+ * @param _discovery_util the discovery util
+ * @param _endpoint the discovered endpoint
+ * @param _object_type_name the object type name
+ * @return UA_StatusCode the status code
+ */
 UA_StatusCode
 discover_and_connect(UA_Client*& _client, discovery_util& _discovery_util, std::string& _endpoint, std::string _object_type_name);
 
