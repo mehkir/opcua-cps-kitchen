c9b974acb5f9426a72e2e0f3ea76b9157662390a
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-09-08 02:12:05 +0200

    Kill http server after completion and pull corrected include in dashboard backend

--- start_scripts/startup_kitchen.bash
+++ start_scripts/startup_kitchen.bash
@@ -1,22 +1,24 @@
 #!/usr/bin/bash
 PROJECT_DIRECTORY=/home/mehmet/vscode-workspaces/opcua-cps-kitchen
 $PROJECT_DIRECTORY/build.bash
+ROBOTS_COUNT=4
 
 $PROJECT_DIRECTORY/build/demos/discovery_server &
 sleep 1
 $PROJECT_DIRECTORY/start_scripts/start_controller.bash &
 sleep 1
-$PROJECT_DIRECTORY/start_scripts/start_conveyor.bash 4 &
+$PROJECT_DIRECTORY/start_scripts/start_conveyor.bash $ROBOTS_COUNT &
 sleep 1
-$PROJECT_DIRECTORY/start_scripts/start_robot.bash 4 &
+$PROJECT_DIRECTORY/start_scripts/start_robot.bash $ROBOTS_COUNT &
 sleep 1
-$PROJECT_DIRECTORY/start_scripts/start_kitchen.bash 4 &
+$PROJECT_DIRECTORY/start_scripts/start_kitchen.bash $ROBOTS_COUNT &
 sleep 1
 cd "$PROJECT_DIRECTORY/cps-kitchen-dashboard"
 export LD_LIBRARY_PATH="$(pwd)/my-addons/open62541/lib"
-node backend.js --robot-count 4 &
+node backend.js --robot-count $ROBOTS_COUNT &
 sleep 1
 python3 -m http.server 8000 &
 # Wait for all background processes to finish
 wait
+kill $(lsof -t -iTCP:8000 -sTCP:LISTEN)
 echo "All startup scripts completed successfully."
