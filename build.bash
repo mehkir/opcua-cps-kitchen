0a72527c91b23242c533b8d87eaf0b1aa21350ca
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-10-03 01:33:29 +0200

    Merge single lines into one with multiple targets

--- build.bash
+++ build.bash
@@ -1,25 +1,33 @@
 #!/usr/bin/bash
 
 cmake -B /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build -S /home/mehmet/vscode-workspaces/opcua-cps-kitchen -DUSE_CUSTOM_VERSION=ON
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target robot_demo -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target clock_demo -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target string_server_for_subscription -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target string_client_for_subscription -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target object_node -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target browse_node -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target tutorial_server_object -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target object_type_node -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target discovery_server -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target client_discovery_lookup -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target client_discovery_lookup_minimal -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target server_method -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target client_call_method -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/tests --config Release --target information_node_tester -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/tests --config Release --target test_now_monotonic -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build --config Release --target start_robot_instance -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build --config Release --target start_controller_instance -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build --config Release --target start_conveyor_instance -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build --config Release --target start_kitchen_instance -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build --config Release --target recipe_testframe -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build --config Release --target session_id_testframe -- -j$(nproc)
-$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build --config Release --target statistics-writer-main -- -j$(nproc)
+$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/demos --config Release --target \
+    robot_demo \
+    clock_demo \
+    string_server_for_subscription \
+    string_client_for_subscription \
+    object_node \
+    browse_node \
+    tutorial_server_object \
+    object_type_node \
+    discovery_server \
+    client_discovery_lookup \
+    client_discovery_lookup_minimal \
+    server_method \
+    client_call_method \
+    -- -j$(nproc)
+
+$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build/tests --config Release --target \
+    information_node_tester \
+    test_now_monotonic \
+    -- -j$(nproc)
+
+$(which cmake) --build /home/mehmet/vscode-workspaces/opcua-cps-kitchen/build --config Release --target \
+    start_robot_instance \
+    start_controller_instance \
+    start_conveyor_instance \
+    start_kitchen_instance \
+    recipe_testframe \
+    session_id_testframe \
+    statistics-writer-main \
+    -- -j$(nproc)
