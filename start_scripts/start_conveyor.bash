c24a1fed2f57260dda195ce17d1e0d99f8e9a397
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-10-21 13:46:30 +0200

    Add log option

--- start_scripts/start_conveyor.bash
+++ start_scripts/start_conveyor.bash
@@ -14,7 +14,8 @@ SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 cd -- "$SCRIPT_DIR"
 cd ..
 PROJECT_DIRECTORY="$(pwd)"
-$PROJECT_DIRECTORY/build/start_conveyor_instance $ROBOTS &
+# $PROJECT_DIRECTORY/build/start_conveyor_instance $ROBOTS &
+"$PROJECT_DIRECTORY/build/start_conveyor_instance" "$ROBOTS" >./logs/conveyor_${ROBOTS}_$(date +%Y%m%d%H%M%S) &
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
     echo "Error: Non-zero exit code detected during conveyor startup. Exiting."
