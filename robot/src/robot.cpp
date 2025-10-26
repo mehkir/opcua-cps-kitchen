80a4bccd8f994730838bdaa57d34c788a1b1b471
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-10-26 02:19:04 +0200

    Call receive_finished_order_notification without any arguments

--- robot/src/robot.cpp
+++ robot/src/robot.cpp
@@ -543,8 +543,6 @@ robot::determine_next_action() {
             is_dish_finished_ = false;
             /* Notify conveyor about finished order */
             method_node_caller receive_finished_order_notification_caller;
-            receive_finished_order_notification_caller.add_scalar_input_argument(&server_endpoint_, UA_TYPES_STRING);
-            receive_finished_order_notification_caller.add_scalar_input_argument(&position_, UA_TYPES_UINT32);
             object_method_info omi = method_id_map_[FINISHED_ORDER_NOTIFICATION];
             size_t output_size = 0;
             UA_Variant* output = nullptr;
@@ -625,8 +623,6 @@ robot::determine_next_action() {
         is_dish_finished_ = true;
         /* Notify conveyor about finished order */
         method_node_caller receive_finished_order_notification_caller;
-        receive_finished_order_notification_caller.add_scalar_input_argument(&server_endpoint_, UA_TYPES_STRING);
-        receive_finished_order_notification_caller.add_scalar_input_argument(&position_, UA_TYPES_UINT32);
         object_method_info omi = method_id_map_[FINISHED_ORDER_NOTIFICATION];
         size_t output_size = 0;
         UA_Variant* output = nullptr;
@@ -1087,8 +1083,6 @@ robot::start() {
                         if (discover_and_connect(conveyor_client_, discovery_util_, conveyor_endpoint, CONVEYOR_TYPE) == UA_STATUSCODE_GOOD) {
                             if (pending_pickup_.load()) {
                                 method_node_caller receive_finished_order_notification_caller;
-                                receive_finished_order_notification_caller.add_scalar_input_argument(&server_endpoint_, UA_TYPES_STRING);
-                                receive_finished_order_notification_caller.add_scalar_input_argument(&position_, UA_TYPES_UINT32);
                                 object_method_info omi = method_id_map_[FINISHED_ORDER_NOTIFICATION];
                                 size_t output_size = 0;
                                 UA_Variant* output = nullptr;
