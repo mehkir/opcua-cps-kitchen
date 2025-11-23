df0a30c75f3b1d83aa23223fbb5ccaf8a4c8168c
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-11-23 23:13:13 +0100

    Use define as return values

--- statistics-writer-main.cpp
+++ statistics-writer-main.cpp
@@ -6,14 +6,14 @@ int main (int argc, char* argv[]) {
     if(argc != 4) {
       std::cerr << "Usage: " + std::string(argv[0]) + " <robot_count>\n";
       std::cerr << "  Example: " + std::string(argv[0]) + " 4\n";
-      return 1;
+      return EXIT_FAILURE;
     }
     std::uint32_t robot_count = std::stoi(argv[1]);
     if (robot_count <= 0) {
       std::cerr << "robot_count must be greater than 0\n";
-      return 1;
+      return EXIT_FAILURE;
     }
     std::unique_ptr<statistics_writer> sw(statistics_writer::get_instance(robot_count));
     sw->write_statistics();
-    return 0;
+    return EXIT_SUCCESS;
 }
