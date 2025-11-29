b0a950502521ba0ded202b5bcfa70f5ac368178c
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-11-29 01:53:32 +0100

    Adjust times

--- time_unit.hpp
+++ time_unit.hpp
@@ -11,14 +11,14 @@
 #include <open62541/types.h>
 
 /* Robot-Agent */
-#define ROBOT_MOVE_TIME 500LL
-#define RECONFIGURATION_TIME 500LL
+#define ROBOT_MOVE_TIME 1000LL
+#define RECONFIGURATION_TIME 1000LL
 #define RETOOLING_TIME 150LL
 #define ACTION_FACTOR 100LL
 
 /* Conveyor-Agent */
-#define DEBOUNCE_TIME 100LL
-#define CONVEYOR_MOVE_TIME 100LL
+#define DEBOUNCE_TIME 500LL
+#define CONVEYOR_MOVE_TIME 500LL
 
 /* Kitchen-Agent */
 #define PlACING_RATE 50LL
