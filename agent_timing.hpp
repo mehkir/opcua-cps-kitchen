6bf6688067f5aa56fccf1898d7a96cb2ac07ccd8
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2026-05-10 02:06:19 +0200

    Reset agent timing to shorter times

--- agent_timing.hpp
+++ agent_timing.hpp
@@ -14,7 +14,7 @@
 #define ROBOT_MOVE_TIME 100LL /**< Time in milliseconds for the robot to move one position on the conveyor belt. */
 #define RECONFIGURATION_TIME 100LL /**< Time in milliseconds for the robot to reconfigure itself to a new capabilities profile. */
 #define RETOOLING_TIME 50LL /**< Time in milliseconds for the robot to retool itself. */
-#define ACTION_FACTOR 100LL /**< Factor for scaling action durations. */
+#define ACTION_FACTOR 10LL /**< Factor for scaling action durations. */
 
 /* Conveyor-Agent */
 #define DEBOUNCE_TIME 100LL /**< Wait time in milliseconds while idling to group closely spaced finished-order notifications before moving. */
