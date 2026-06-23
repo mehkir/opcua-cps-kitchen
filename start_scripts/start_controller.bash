dbd597261003f12c66277e635f46e8361e1657b9
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2026-06-24 01:14:01 +0200

    Refactor startup scripts to use exec for better process management and remove error handling

--- start_scripts/start_controller.bash
+++ start_scripts/start_controller.bash
@@ -2,10 +2,5 @@
 SCRIPT_PATH="$(realpath "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 PROJECT_DIRECTORY="$(cd -- "$SCRIPT_DIR/.." && pwd)"
-$PROJECT_DIRECTORY/build/start_controller_instance &
-# "$PROJECT_DIRECTORY/build/start_controller_instance" >./logs/controller_$(date +%Y%m%d%H%M%S) &
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-    echo "Error: Non-zero exit code detected during controller startup. Exiting."
-    exit $exit_code
-fi
+exec "$PROJECT_DIRECTORY/build/start_controller_instance"
+# exec "$PROJECT_DIRECTORY/build/start_controller_instance" >./logs/controller_$(date +%Y%m%d%H%M%S)
